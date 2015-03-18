9cb9661eaafb3ba4ac8ff594d8d0fd355bde232a
lykmapipo <lallyelias87@gmail.com>
2015-03-19 00:21:06 +0300

    adding grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,12 @@ module.exports = function(grunt) {
 
     //grunt tasks config
     grunt.initConfig({
+        lykmapipo: {
+            posts: '_posts',
+            site: 'public',
+            drafts: '_drafts'
+        }
+        //metalsmith config
         metalsmith: {
             lykmapipo: {
                 options: {
@@ -43,6 +49,87 @@ module.exports = function(grunt) {
                 src: '_posts',
                 dest: '_site'
             }
+        },
+
+        //connect config
+        connect: {
+            options: {
+                port: 9000,
+                // Change this to '0.0.0.0' to access the server from outside.
+                hostname: 'localhost',
+                livereload: 35729
+            },
+
+            livereload: {
+                options: {
+                    open: true
+                },
+                middleware: function(connect) {
+                    return [
+                        connect.static('<%= lykmapipo.site %>'),
+                        connect().use(
+                            '/bower_components',
+                            connect.static('./bower_components')
+                        )
+                    ];
+                }
+            }
+        },
+
+        //watch files for changes
+        // Watches files for changes and runs tasks based on the changed files
+        watch: {
+            _posts: {
+                files: ['<%= lykmapipo.posts %>/**/*'],
+                tasks: ['metalsmith:lykmapipo'],
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                }
+            },
+            gruntfile: {
+                files: ['Gruntfile.js'],
+                tasks: ['metalsmith:lykmapipo'],
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                }
+            },
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    '<%= lykmapipo.posts %>/**/*'
+                ]
+            }
+        },
+
+        // Empties folders to start fresh
+        clean: {
+            lykmapipo: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '.tmp',
+                        '<%= lykmapipo.site %>/{,*/}*',
+                        '!<%= lykmapipo.site %>/.git{,*/}*'
+                    ]
+                }]
+            }
         }
+
     });
+
+    grunt.registerTask('serve', 'Compile then start a connect web server', function(target) {
+
+        grunt.task.run([
+            'clean:lykmapipo'
+            'metalsmith:lykmapipo',
+            'connect:livereload',
+            'watch'
+        ]);
+    });
+
+    grunt.registerTask('build', [
+        'metalsmith:lykmapipo'
+    ]);
 }
